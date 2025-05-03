eeaaea772fbed766bf13855ccf93032f801ef2a6
Murylo Anjos <muryloanjos51@gmail.com>
2025-05-03 15:50:27 -0300

    Project Add

---
+++ src/main/java/br/com/calmaja/CalmajaApplication.java
@@ -0,0 +1,13 @@
+package br.com.calmaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/calmaja/CalmajaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calmaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
